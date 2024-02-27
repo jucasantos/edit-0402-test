6c2b2be5081803a6d779857fcce3f55c878f6222
jucasantos <juliadecarvalhosantos40@gmail.com>
2024-02-27 12:53:31 +0000

    tarefa #1

--- polls/routes.js
+++ polls/routes.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const services = require("./services");
+const { getDB } = require("../db/mongodb");
 
 router.get("/", async (req, res) => {
   const polls = await services.getAllPolls();
   res.status(200).json(polls);
+});
+
+//tarefa 1
+
+router.get("/:id", async (req, res) => {
+  const pollId = req.params.id;
+
+  const poll = await services.getPollById(pollId);
+  if (!poll) {
+    return res.status(404).json({ error: "not found" });
+  }
+
+  res.status(200).json(poll);
 });
 
 router.delete("/:id", async (req, res) => {
